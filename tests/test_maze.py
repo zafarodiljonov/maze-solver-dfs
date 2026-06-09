0d8d70b2688f090c0f56f2b57dce9a1091de3d84
zafarodiljonov <odijonovzafar@gmail.com>
2026-06-09 17:42:13 +0500

    Add failing test for DFS path solving

--- tests/test_maze.py
+++ tests/test_maze.py
@@ -30,3 +30,26 @@ def test_outside_maze_is_invalid_move():
     assert is_valid_move(maze, 0, -1) is False
     assert is_valid_move(maze, 3, 0) is False
     assert is_valid_move(maze, 0, 3) is False
+
+from src.maze import solve_maze_dfs
+
+
+def test_dfs_finds_path_from_start_to_end():
+    maze = [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 0, 0],
+    ]
+
+    start = (0, 0)
+    end = (2, 2)
+
+    path = solve_maze_dfs(maze, start, end)
+
+    assert path == [
+        (0, 0),
+        (1, 0),
+        (2, 0),
+        (2, 1),
+        (2, 2),
+    ]
